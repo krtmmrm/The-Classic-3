1cc186fe88fb2131902f0d6834252b8a64a58eea
krtmmrm <krtm.mrm@gmail.com>
2021-02-22 17:19:56 +0100

    Routes added

--- static/controller/index.js
+++ static/controller/index.js
@@ -1,3 +1,54 @@
+//
+const navigateTo = url => {
+    history.pushState(null, null , url);
+    router();
+}
+
+//
+const router = async () => {
+    const routes = [
+        { path: "/", view: () => console.log("Home page") },
+        { path: "/about", view: () => console.log("About page") },
+        { path: "/projects", view: () => console.log("Projects page") },
+        { path: "/faq", view: () => console.log("Faq page") },
+        { path: "/register", view: () => console.log("Register page") }
+    ];
+
+    //Test each route for potencial matches 
+    const potentialMatches = routes.map(route => {
+        return {
+            route:route,
+            isMatch: location.pathname === route.path
+        };
+    });
+
+   
+
+    let match = potentialMatches.find(potentialMatch => potentialMatch.isMatch);
+
+    if (!match) {
+        match = {
+            route: routes[0],
+            result: [location.pathname]
+        };
+    };
+
+    console.log(match.route.view());
+};
+
+//Allows to go back and forth 
+window.addEventListener("popstate", router);
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.body.addEventListener("click", e => {
+        if (e.target.matches("[data-link]")) {
+            e.preventDefault();
+            navigateTo(e.target.href);
+        }
+    });
+});
+
+//Small Screen Navbar
 const navSlide = () =>{
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav_links');
