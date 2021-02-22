c17e59b34f9ba5997b6ec4a079c88485d0456201
krtmmrm <krtm.mrm@gmail.com>
2021-02-22 12:47:05 +0100

    Router added

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use("/static", express.static(path.resolve(__dirname,"static")));
+
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "view", "index.html"));
+})
+
+app.listen(process.env.PORT || 3040 , () => console.log("Server running..."));
